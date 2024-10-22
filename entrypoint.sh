75d1e25be741924cff7a4c8e931733c88e35c692
tranvietphuoc <phuoctv.ut@gmail.com>
2024-10-23 00:46:33 +0700

    edit entrypoint to run web container

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,27 @@ echo 'waiting for postgres...'
 
 while ! nc -z $DB_HOST $DB_PORT; do
 
-    sleep 0.1
+    sleep 1
 
+done
+
+
+# Wait for Redis to be ready
+until nc -z redis 6379; do
+  echo "Waiting for Redis..."
+  sleep 1
 done
 
 echo 'PostgreSQL started'
 
 echo 'running migration...'
-# python manage.py makemigrations
+python manage.py makemigrations
 python manage.py migrate --fake-initial
 
 echo 'collecting static files...'
 python manage.py collectstatic --no-input
+
+echo 'running server...'
+python manage.py runserver 0.0.0.0:8000
 
 exec "$@"
